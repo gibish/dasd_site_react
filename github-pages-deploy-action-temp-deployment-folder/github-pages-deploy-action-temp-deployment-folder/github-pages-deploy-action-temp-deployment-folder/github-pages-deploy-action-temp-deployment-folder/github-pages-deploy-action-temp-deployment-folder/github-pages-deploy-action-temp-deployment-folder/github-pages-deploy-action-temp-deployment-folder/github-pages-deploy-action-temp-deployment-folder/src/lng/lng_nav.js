b5405f57b0a6182c3a959b46da44501c741a9c7b
gibish <98774747+gibish@users.noreply.github.com>
2023-11-24 12:05:30 +0000

    Deploying to gh-pages from @ gibish/dasd_site_react@23f2a26059cc3a8999ca7adf516efc15796fa507 🚀

---
+++ github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/src/lng/lng_nav.js
@@ -0,0 +1,43 @@
+﻿﻿// U+FEFF at the beginning
+
+/*eslint unicode-bom: ["error", "always"]*/
+
+const navText = {
+  "nav-main": {
+    en: "Main",
+    ua: "Головна",
+  },
+  "nav-department": {
+    en: "About department",
+    ua: "Про відділ",
+  },
+  "nav-institute": {
+    en: "About institute",
+    ua: "Про інститут",
+  },
+  "nav-projects": {
+    en: "Projects",
+    ua: "Проекти",
+  },
+  "nav-staff": {
+    en: "Staff",
+    ua: "Співробітники",
+  },
+  "nav-publications": {
+    en: "Publications",
+    ua: "Публікації",
+  },
+  "nav-contacts": {
+    en: "Contacts",
+    ua: "Контакти",
+  },
+};
+
+export default navText;
+
+/*
+"":{
+  en: "",
+  ua: "",
+},
+*/
---
+++ github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/src/lng/lng_publications.js
@@ -0,0 +1,23 @@
+﻿// U+FEFF at the beginning
+
+/*eslint unicode-bom: ["error", "always"]*/
+
+const publicationsText = {
+  "publ-content-welcome": {
+    en: "Selected Publications",
+    ua: "Вибрані публікації",
+  },
+  "publ-content-1": {
+    en: "The list of selected publications is generated automatically on the basis of the international ORCID system.",
+    ua: "Перелік вибраних публікацій сформовано в автоматичному режимі на базі міжнародної системи ORCID.",
+  },
+};
+
+export default publicationsText;
+
+/*
+  "":{
+    en: "",
+    ua: "",
+  },
+  */
