881a553398ae2a20d4acaac95ee1c33740b3b4dd
Igor Galelyuka <galelyuka@gmail.com>
2023-11-30 11:06:43 +0200

    1130 1

--- src/components/projectSmall/ProjectSmall.js
+++ src/components/projectSmall/ProjectSmall.js
@@ -1,13 +1,14 @@
 import { NavLink } from "react-router-dom";
 
 function ProjectSmall({ project, index }) {
+  const projectLink = `/project0${index + 1}`;
   return (
     <li className="content__descr-img content__descr-img-proj content--dotted-bottom">
-      <NavLink to={`/project/${index}`}>
+      <NavLink to={projectLink}>
         <img className="content__img content__img-proj" src={project.img} alt={project.name} />
       </NavLink>
       <div className="content__descr">
-        <NavLink to={`/project/${index}`} className="content__site">
+        <NavLink to={projectLink} className="content__site">
           <span className="content__descr-span span-name-a content__descr--head" data-lang="proj-name">
             {project.name}
           </span>
@@ -15,7 +16,7 @@ function ProjectSmall({ project, index }) {
         <span className="content__descr-span content--justify" data-lang="proj-descr">
           {project.descr}
         </span>
-        <NavLink to={`/project/${index}`} className="content__site">
+        <NavLink to={projectLink} className="content__site">
           <span className="content__site-text" data-lang="proj-detail">
             {project.detail}
           </span>
--- src/pages/Project.js
+++
@@ -1,9 +0,0 @@
-import { useParams } from "react-router-dom";
-
-function Project() {
-  const { id } = useParams();
-
-  return <div>{id}</div>;
-}
-
-export default Project;
